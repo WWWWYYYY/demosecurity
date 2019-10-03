a55ba237dd07039bc5eb9d3f7e408f7f69c3104a
wangyi <394489664@qq.com>
2019-10-03 09:37:24 +0800

    （*） 包含功能： 0、全局获取用户信息的工具类

--- src/main/java/com/example/demosecurity/web/IndexResource.java
+++ src/main/java/com/example/demosecurity/web/IndexResource.java
@@ -1,6 +1,10 @@
 package com.example.demosecurity.web;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexResource {
 
     @GetMapping(value = {"/index"})
-    public String index(){
+    public String index(ModelMap map){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Object principal = authentication.getPrincipal();
+        if (principal!=null){
+            UserDetails userDetails = (UserDetails) principal;
+            map.addAttribute("username",userDetails.getUsername());
+            map.addAttribute("password",userDetails.getPassword());
+            map.addAttribute("authorities",userDetails.getAuthorities());
+        }
         return "index";
     }
 
